1284250d08c4cac37e11e1c58644af9c71517302
Michael Marcenich <info@m-squared-solutions.it>
2019-10-15 18:46:37 +0200

    test(jest): setup jest to use typescript

--- jest.config.js
+++ jest.config.js
@@ -5,14 +5,14 @@ module.exports = {
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
   transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '^.+\\.tsx?$': 'ts-jest',
+    '.*\\.vue$': 'vue-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/(plugins|modules|assets|store|middleware)/**/*.[tj]s',
+    '<rootDir>/(layouts|components|pages)/**/*.vue'
   ]
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,111 +3,111 @@ import { Configuration } from '@nuxt/types'
 import colors from 'vuetify/es5/util/colors'
 
 const config: Configuration = {
-    mode: 'spa',
-    srcDir: 'resources/nuxt',
+  mode: 'spa',
+  srcDir: 'resources/nuxt',
 
-    /*
-     ** Headers of the page
-     */
-    head: {
-        titleTemplate: '%s - ' + process.env.npm_package_name,
-        title: process.env.npm_package_name || '',
-        meta: [
-            { charset: 'utf-8' },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
-            },
-            {
-                hid: 'description',
-                name: 'description',
-                content: process.env.npm_package_description || ''
-            }
-        ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-    },
-
-    /*
-     ** Customize the progress-bar color
-     */
-    loading: { color: '#fff' },
-
-    /*
-     ** Global CSS
-     */
-    css: [],
-
-    /*
-     ** Plugins to load before mounting the App
-     */
-    plugins: [],
-
-    /*
-     ** Nuxt.js build modules
-     */
-    buildModules: [
-        '@nuxt/typescript-build',
-        // Doc: https://github.com/nuxt-community/eslint-module
-        '@nuxtjs/eslint-module',
-        '@nuxtjs/vuetify'
+  /*
+   ** Headers of the page
+   */
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
 
-    /*
-     ** Nuxt.js modules
-     */
-    modules: [
-        'nuxt-laravel',
-        // Doc: https://axios.nuxtjs.org/usage
-        '@nuxtjs/axios'
-    ],
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
 
-    /*
-     ** Axios module configuration
-     ** See https://axios.nuxtjs.org/options
-     */
-    axios: {},
+  /*
+   ** Global CSS
+   */
+  css: [],
 
-    typescript: {
-        typeCheck: {
-            eslint: true
-        }
-    },
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
-    /*
-     ** vuetify module configuration
-     ** https://github.com/nuxt-community/vuetify-module
-     */
-    vuetify: {
-        customVariables: ['~/assets/variables.scss'],
-        theme: {
-            dark: true,
-            themes: {
-                dark: {
-                    primary: colors.blue.darken2,
-                    accent: colors.grey.darken3,
-                    secondary: colors.amber.darken3,
-                    info: colors.teal.lighten1,
-                    warning: colors.amber.base,
-                    error: colors.deepOrange.accent4,
-                    success: colors.green.accent3
-                }
-            }
-        }
-    },
+  /*
+   ** Nuxt.js build modules
+   */
+  buildModules: [
+    '@nuxt/typescript-build',
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
+  ],
 
-    router: {
-        base: '/app/'
-    },
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    'nuxt-laravel',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios'
+  ],
 
-    /*
-     ** Build configuration
-     */
-    build: {
-        /*
-         ** You can extend webpack config here
-         */
-        // extend(config, ctx) {}
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+
+  typescript: {
+    typeCheck: {
+      eslint: true
     }
+  },
+
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  router: {
+    base: '/app/'
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    // extend(config, ctx) {}
+  }
 }
 
 export default config
