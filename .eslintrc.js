a187949b9d0a526968a636d404716da3729917bf
Gautier <gautier.deleglise@ediservices.com>
2020-09-10 15:57:23 +0200

    :arrow_up: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true
-    },
-    parserOptions: {
-        parser: 'babel-eslint'
-    },
-    extends: [
-        '@nuxtjs',
-        'prettier',
-        'prettier/standard',
-        'prettier/unicorn',
-        'prettier/vue',
-        'plugin:prettier/recommended',
-        'plugin:nuxt/recommended'
-    ],
-    plugins: ['prettier'],
-    // add your custom rules here
-    rules: {}
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/standard',
+    'prettier/unicorn',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {}
 }
--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    rootDir: 'resources/nuxt',
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/$1',
-        '^~/(.*)$': '<rootDir>/$1',
-        '^vue$': 'vue/dist/vue.common.js'
-    },
-    moduleFileExtensions: ['js', 'vue', 'json'],
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-        '.*\\.(vue)$': 'vue-jest'
-    },
-    collectCoverage: true,
-    collectCoverageFrom: [
-        '<rootDir>/(assets|middleware|plugins|modules|store)/**/*.js',
-        '<rootDir>/(layouts|pages|components)/**/*.vue'
-    ]
+  rootDir: 'resources/nuxt',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: ['js', 'vue', 'json'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/(assets|middleware|plugins|modules|store)/**/*.js',
+    '<rootDir>/(layouts|pages|components)/**/*.vue'
+  ]
 }
