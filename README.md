6ac4f6d948297bd2aecdee822e52d535d7db6385
Gautier <gautier.deleglise@gmail.com>
2020-09-12 17:35:42 +0200

    :sparkles: Laranuxt - prepared package as a global solution

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# Nuxt Laravel Starter
+# Laranuxt
 
-Laravel framework with integrated NuxtJs support, preconfigured for eslint, jest and vuetify.
+<p align="center">
+  <a href="https://laravel.com"><img alt="Laravel" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png" width="92" height="92" /></a>
+  <a href="https://vuejs.org"><img alt="Vue.js" src="https://vuejs.org/images/logo.png" width="92" height="92" /></a>
+  <a href="https://vuetifyjs.com/"><img alt="Vuetify" src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" width="92" height="92" /></a>
+  <a href="https://eslint.org/"><img alt="ESLint" src="https://d33wubrfki0l68.cloudfront.net/204482ca413433c80cd14fe369e2181dd97a2a40/092e2/assets/img/logo.svg" width="92" height="92" /></a>
+  <a href="https://jestjs.io/"><img alt="Jest" src="https://miro.medium.com/max/600/1*i37IyHf6vnhqWIA9osxU3w.png" width="92" height="92" /></a>
+</p>
+
+## Laravel + Nuxt.js template
+
+[![](https://img.shields.io/badge/nuxt.js-v2.14.5-04C690.svg)](https://nuxtjs.org)
+[![](https://img.shields.io/badge/Laravel-v8.0.3-ff2e21.svg)](https://laravel.com)
+[![](https://img.shields.io/badge/Vuetify-v2.3.10-1697F6.svg)](https://laravel.com)
 
 For typescript support checkout the [`typescript`](https://github.com/m2sd/nuxt-laravel-starter/tree/typescript) branch.
 
--- resources/nuxt/pages/index.vue
+++ resources/nuxt/pages/index.vue
@@ -1,6 +1,36 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
+  <v-row column justify="center" align="center">
+    <v-col cols="12">
+      <v-card>
+        <v-card-title>
+          Laranuxt
+        </v-card-title>
+        <v-card-text>
+          Laranuxt is powered by open source. Please consider contributing to
+          the project to help us maintaining the solution.
+        </v-card-text>
+        <v-card-title>
+          Contributers:
+        </v-card-title>
+        <v-card-text>
+          <a
+            v-for="(contributer, index) in contributers"
+            :key="index"
+            :href="contributer.link"
+            target="_blank"
+            rel="noopener"
+          >
+            <v-img
+              width="80"
+              height="80"
+              class="d-inline-block rounded-circle mr-1"
+              :src="contributer.img"
+            />
+          </a>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12">
       <div class="text-center">
         <logo />
         <vuetify-logo />
@@ -76,10 +106,27 @@
           <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
         </v-card-actions>
       </v-card>
-    </v-flex>
-  </v-layout>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      contributers: [
+        {
+          img:
+            'https://media-exp1.licdn.com/dms/image/C4E03AQFdWa89vQYAUQ/profile-displayphoto-shrink_800_800/0?e=1605139200&v=beta&t=OALhz2ddqPu9cNB4-l7Fem396XZ5jV_HoJ8rNrxbNmw',
+          link: 'https://github.com/mentAl-maZe'
+        },
+        {
+          img:
+            'https://www.gravatar.com/avatar/fab898b83ca79bd9ca003b8630771204?s=80',
+          link: 'https://github.com/GautierDele'
+        }
+      ]
+    }
+  }
+}
 </script>
