9b490a4f1ba2ad3d3d317172958cbc047a4602ec
Gautier <gautier.deleglise@gmail.com>
2020-09-20 10:42:46 +0200

    :recycle: removed commit lint on the template package

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/main/Repos/Projects/nuxt-laravel-starter/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/main/Repos/Projects/nuxt-laravel-starter/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\nuxt-laravel-starter\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\nuxt-laravel-starter\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
